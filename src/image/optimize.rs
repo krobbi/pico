0bc18d2e0aa87039d13563e5fd04742e7c43ab8f
krobbi <70552146+krobbi@users.noreply.github.com>
2023-11-28 23:19:29 +0000

    Implement optimizations

--- src/image/optimize.rs
+++ src/image/optimize.rs
@@ -1,9 +1,29 @@
+extern crate oxipng;
+
 use super::Image;
+
+use oxipng::{Options, StripChunks};
 
 impl Image {
     /// Create an optimized copy of the image.
     pub fn optimize(&self) -> Image {
-        println!("TODO: Implement optimization.");
-        Image::from_data(self.data.clone()).unwrap()
+        let options = get_options();
+
+        match oxipng::optimize_from_memory(self.data.as_slice(), &options) {
+            Ok(data) => Image::from_data(data).unwrap(),
+            Err(error) => {
+                eprintln!("{}", error);
+                Image::from_data(self.data.clone()).unwrap()
+            }
+        }
     }
+}
+
+/// Get optimization options.
+fn get_options() -> Options {
+    let mut options = Options::max_compression();
+    options.optimize_alpha = true;
+    options.strip = StripChunks::All;
+    options.fast_evaluation = false;
+    options
 }
