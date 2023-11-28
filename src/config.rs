5c1f3003b4558497ea8eaad01cb8fdda93ead3f8
krobbi <70552146+krobbi@users.noreply.github.com>
2023-11-28 16:40:21 +0000

    Refactor and format

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,27 @@
+use std::path::PathBuf;
+
+use clap::{arg, command, ArgMatches};
+
 /// Configuration data for Pico.
 pub struct Config {
     /// The path to the source PNG file to read.
-    pub source_path: String,
+    pub source_path: PathBuf,
 }
 
 impl Config {
     /// Create a new config instance from command line arguments.
-    pub fn from_args() -> Self {
-        use clap::{arg, command};
-        
-        let mut matches = command!()
+    pub fn from_args() -> Config {
+        let args = command!()
             .arg(arg!(<source> "The source PNG file to read"))
             .get_matches();
-        
-        let source_path = matches
-            .remove_one::<String>("source")
-            .unwrap();
-        
+
         Config {
-            source_path,
+            source_path: get_arg_path(&args, "source"),
         }
     }
+}
+
+/// Get a path from parsed arguments by its ID.
+fn get_arg_path(args: &ArgMatches, id: &str) -> PathBuf {
+    args.get_one::<String>(id).unwrap().into()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 mod config;
 
+use config::Config;
+
 /// Display the configured source path.
 fn main() {
-    let config = config::Config::from_args();
-    println!("Source path: {}", config.source_path);
+    let config = Config::from_args();
+    println!("Source file: {}", config.source_path.display());
+
+    if config.source_path.is_file() {
+        println!("Source file exists!");
+    } else {
+        println!("Source file does not exist!");
+    }
 }
